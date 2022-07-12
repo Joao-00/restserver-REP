36bebd3a484b0ed995f503a8e36346413f802c3e
Joao Ormeño <joao23.1996@gmail.com>
2022-07-11 23:41:53 -0400

    trabajando: controllers

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,43 +1,52 @@
-const { response } = require('express');
+const { response, request } = require('express');
 
 
-const usuariosGet = (req, res) => {
+const usuariosGet = (req= request, res = response) => {
+
+    const {q, nombre, apikey} = req.query;
+
     res.json({
-        msg: 'get API - controlador'
+        msg: 'get API - controlador',
+        q,
+        nombre,
+        apikey
     });
-
 }
 
-const usuariosPut = (req, res) => {
-    res.json({
-        msg: 'put API - controlador'
-    });
-
-}
 
 const usuariosPost = (req, res) => {
-
+    
     const body = req.body;
-
+    
     res.json({
         msg: 'post API - controlador',
         body
     });
-
 }
+
+
+const usuariosPut = (req, res) => {
+    
+    const id = req.params.id;
+
+    res.json({
+        msg: 'put API - controlador',
+        id
+    });
+}
+
 
 const usuariosDelete = (req, res) => {
     res.json({
         msg: 'delete API - controlador'
     });
-
 }
+
 
 const usuariosPatch = (req, res) => {
     res.json({
         msg: 'patch API - controlador'
     });
-
 }
 
 
