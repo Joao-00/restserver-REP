e74de9556ac433b5ceec7a5e2de61aa67d3b4f86
Joao Ormeño <joao23.1996@gmail.com>
2022-07-11 23:25:29 -0400

    add: controllers/usuarios && routes/usuarios

---
+++ controllers/usuarios.js
@@ -0,0 +1,48 @@
+const { response } = require('express');
+
+
+const usuariosGet = (req, res) => {
+    res.json({
+        msg: 'get API - controlador'
+    });
+
+}
+
+const usuariosPut = (req, res) => {
+    res.json({
+        msg: 'put API - controlador'
+    });
+
+}
+
+const usuariosPost = (req, res) => {
+    res.json({
+        msg: 'post API - controlador'
+    });
+
+}
+
+const usuariosDelete = (req, res) => {
+    res.json({
+        msg: 'delete API - controlador'
+    });
+
+}
+
+const usuariosPatch = (req, res) => {
+    res.json({
+        msg: 'patch API - controlador'
+    });
+
+}
+
+
+
+
+module.exports = {
+    usuariosGet,
+    usuariosPut,
+    usuariosPost,
+    usuariosDelete,
+    usuariosPatch
+}
