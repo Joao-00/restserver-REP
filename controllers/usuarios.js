4b4ea9585c5b20a031d692626c5ccb9714494284
Joao Ormeño <joao23.1996@gmail.com>
2022-07-15 18:29:23 -0400

    trabajando: controllers/uusuarios && routes/usuarios  | validando correo

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,5 +1,6 @@
 const { response, request } = require('express');
 const bcryptjs = require('bcryptjs');
+const {validationResult} = require('express-validator');
 
 const Usuario = require('../models/usuario');
 
@@ -18,19 +19,29 @@ const usuariosGet = (req= request, res = response) => {
 
 const usuariosPost = async(req, res) => {
     
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return req.status(400).json(errors);
+    }
+
+
     const {nombre, correo, password, rol} = req.body;
     const usuario = new Usuario( {nombre, correo, password, rol} );
     
     //verificar si el correo existe
-
-
+    const existeEmail = await Usuario.findOne({correo});
+    if (existeEmail) {
+        return res.status(400).json({
+            msg: 'El correo ya esta registrado'
+        });
+    }
     //encriptar password
     const salt = bcryptjs.genSaltSync();
     usuario.password = bcryptjs.hashSync(password, salt);
 
     //guardar db
     await usuario.save();
-    
+
     res.json({
         usuario
     });
