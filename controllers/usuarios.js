3bf9f6840477c00b2ab3a6c67c336bfc8510d048
Joao Ormeño <joao23.1996@gmail.com>
2022-07-22 23:56:01 -0400

    trabajando: get usuarios/controllers

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -4,15 +4,22 @@ const bcryptjs = require('bcryptjs');
 
 const Usuario = require('../models/usuario');
 
-const usuariosGet = (req= request, res = response) => {
+const usuariosGet = async (req= request, res = response) => {
 
-    const {q, nombre, apikey} = req.query;
+    //const {q, nombre, apikey} = req.query;
+    const { limite = 5, desde = 0 } = req.query;
+    const query = {estado: true};
+
+    const {total, usuarios} = await Promise.all([
+        Usuario.countDocuments(query),
+        Usuario.find(query)
+            .skip(Number(desde))
+            .limit(Number(limite))
+    ])
 
     res.json({
-        msg: 'get API - controlador',
-        q,
-        nombre,
-        apikey
+        total,
+        usuarios
     });
 }
 
