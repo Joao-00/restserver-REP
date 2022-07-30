604b772b27591e3a9cfe3ce5c9ffd726b5d35793
Joao Ormeño <joao23.1996@gmail.com>
2022-07-29 21:33:50 -0400

    get arreglado

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -8,14 +8,14 @@ const usuariosGet = async (req= request, res = response) => {
 
     //const {q, nombre, apikey} = req.query;
     const { limite = 5, desde = 0 } = req.query;
-    const query = {estado: true};
+    const query = {estado: true}; 
 
-    const {total, usuarios} = await Promise.all([
+    const [total, usuarios] = await Promise.all([
         Usuario.countDocuments(query),
         Usuario.find(query)
             .skip(Number(desde))
             .limit(Number(limite))
-    ])
+    ]);
 
     res.json({
         total,
@@ -66,7 +66,7 @@ const usuariosDelete = async (req, res) => {
 
     //fisicamente lo boramos
     //const usuario = await Usuario.findByIdAndDelete(id);
-    
+
     const usuario = await Usuario.findByIdAndUpdate(id, {estado: false});
 
     res.json(usuario);
